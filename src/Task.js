ad351f28ca4341702cf640becd033de7b2f7e22b
Alexandre Shafii <alexandre.shafii@gmail.com>
2020-12-02 13:01:06 +0100

    chore: removed elements for deployment

--- src/Task.js
+++ src/Task.js
@@ -2,17 +2,23 @@ import React from "react";
 import firebase from "firebase/app";
 
 export const Task = ({ task }) => {
-  const [taskName, setTaskName] = React.useState(task.taskName);
-  const [taskOwner, setTaskOwner] = React.useState(task.taskOwner);
-  const [taskDeadline, setTaskDeadline] = React.useState(task.taskDeadline);
-  const [taskQuantity, setTaskQuantity] = React.useState(task.quantity);
-  // const onUpdate = () => {
-  //   const db = firebase.firestore();
-  //   db.collection("things")
-  //     .doc(task.id)
-  //     .set({ ...task, taskName });
-  //   console.log({ ...task });
-  // };
+  const [taskName] = React.useState(task.taskName);
+  const [taskOwner] = React.useState(task.taskOwner);
+  const [taskDeadline] = React.useState(task.taskDeadline);
+  const [taskQuantity] = React.useState(task.quantity);
+
+  // ----> will be useful for modifying
+  // // const [taskName, setTaskName] = React.useState(task.taskName);
+  // // const [taskOwner, setTaskOwner] = React.useState(task.taskOwner);
+  // // const [taskDeadline, setTaskDeadline] = React.useState(task.taskDeadline);
+  // // const [taskQuantity, setTaskQuantity] = React.useState(task.quantity);
+  // // const onUpdate = () => {
+  // //   const db = firebase.firestore();
+  // //   db.collection("things")
+  // //     .doc(task.id)
+  // //     .set({ ...task, taskName });
+  // //   console.log({ ...task });
+  // // };
 
   const onDelete = () => {
     const db = firebase.firestore();
